b1baf26a4d84fc927ce893963a0908381b98fbe4
William Miller <wmiller65536@gmail.com>
2019-01-04 15:57:12 -0700

    Makefile updates and moved BulirschStoer class into separate header

---
+++ src/bulirsch-stoer.ode.h
@@ -0,0 +1,24 @@
+#ifndef BULIRSCH_STOER_ODE_H
+#define BULIRSCH_STOER_ODE_H
+#include "csim.h"
+
+class BulirschStoer : public CSim
+{
+public:
+	BulirschStoer();
+	double step(double (*f)(double, double), double y0, double x0, double x, int nsteps);
+	int simulate(double (*f)(double, double), double y0, double* y1, double x, double h, double* h_new, 
+		double epsilon, double yscale, int rational_extrapolate);
+	static int rationalExtrapolation(double* fzero, double* tableau, double* x, double f, int n);
+	static int polynomialExtrapolation(double* fzero, double* tableau, double* x, double f, int n);
+
+	void setThreading(threadmode t);
+	threadmode getThreading();
+
+private:
+	static int attempts;
+	static int steps[];
+	threadmode threading;
+};
+
+#endif // BULITRSCH_STOER_ODE_H
